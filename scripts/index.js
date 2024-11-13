bb772707fbed4bf59d5bc078e54fbbd1a7ba3d3f
Tyler Nielsen <tynielsen@gmail.com>
2024-11-12 18:37:29 -0700

    mainly getting themes set up and some minor tweaks to layout and content

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,43 @@
+// Code Snippets 
 hljs.highlightAll();
 
+// Setting copyright to current year
 document.querySelector(".year").textContent = (new Date().getFullYear());
+
+
+/** *********************************************
+ *  Theme Functionality
+********************************************* **/
+
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+// function toggleTheme() {
+//   if (localStorage.getItem('theme') === 'theme-light'){
+//       setTheme('theme-light');
+//   } else {
+//       setTheme('theme-seasonal');
+//   }
+// }
+
+// // function to toggle between light and dark theme
+// function toggleTheme() {
+//   if (localStorage.getItem('theme') === 'theme-dark'){
+//       setTheme('theme-dark');
+//   } else {
+//       setTheme('theme-seasonal');
+//   }
+// }
+
+// // Immediately invoked function to set the theme on initial load
+// (function () {
+//   if (localStorage.getItem('theme') === 'seasonal') {
+//       setTheme('seasonal');
+//   } else {
+//       setTheme('seasonal');
+//   }
+// })();
